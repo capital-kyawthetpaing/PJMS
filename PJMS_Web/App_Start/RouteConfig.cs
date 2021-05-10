9529eb9cb627ffd3ce62a1af8ed14c527be83ff3
Htet Htet Win <capital.htethtetwin@gmail.com>
2021-05-10 09:05:33 -0700

    hhw

--- PJMS_Web/App_Start/RouteConfig.cs
+++ PJMS_Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PJMS_Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Project", action = "ProjectList", id = UrlParameter.Optional }
+                defaults: new { controller = "Project", action = "ProjectTeamSetUp", id = UrlParameter.Optional }
             );
         }
     }
