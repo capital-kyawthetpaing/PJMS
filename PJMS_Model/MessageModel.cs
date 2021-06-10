a390de3abb10a995216c50b51aa071ba76522418
Swe Swe Aung <capital.swesweaung@gmail.com>
2021-06-10 11:34:41 +0630

    by ssa [Message_ID]

---
+++ PJMS_Model/MessageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using PJMS_Model;
+
+namespace PJMS_Model
+{
+    public class MessageModel: BaseModel
+    {
+        public string MessageID { get; set; }
+        public string MessageText1 { get; set; }
+        public string MessageButton { get; set; }
+    }
+}
