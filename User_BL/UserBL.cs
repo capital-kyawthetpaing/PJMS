844bcf494580e9b1001f66d89b50f20853f37e93
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2021-06-11 17:13:57 +0630

    ktp

---
+++ User_BL/UserBL.cs
@@ -0,0 +1,24 @@
+﻿using CKM_CommonFunction;
+using CKM_DataLayer;
+using System.Data.SqlClient;
+using PJMS_Model;
+namespace User_BL
+{
+    public class UserBL
+    {
+        CKMDL cKMDL;
+        FileFunction ff;
+        public UserBL()
+        {
+            cKMDL = new CKMDL();
+            ff = new FileFunction();
+        }
+        public string GetUser(UserModel userModel)
+        {
+            userModel.Sqlprms = new SqlParameter[2];
+            userModel.Sqlprms[0] = new SqlParameter("@UserID", userModel.UserID);
+            userModel.Sqlprms[1] = new SqlParameter("@Password", userModel.Password);
+            return cKMDL.SelectJson("User_Select", ff.GetConnectionWithDefaultPath("PJMS"), userModel.Sqlprms);
+        }
+    }
+}
